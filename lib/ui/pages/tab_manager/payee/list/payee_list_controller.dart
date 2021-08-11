fc7b2d5360ec2a0e6fb0944e080248116198178f
Jonathan Byron <jonathan.byron2908@gmail.com>
2021-08-11 15:46:42 +0200

    Fix payee list

--- lib/ui/pages/tab_manager/payee/list/payee_list_controller.dart
+++ lib/ui/pages/tab_manager/payee/list/payee_list_controller.dart
@@ -1,10 +1,18 @@
 import 'package:my_finance_flutter_3/core/domain/model/payee/payee_model.dart';
 import 'package:my_finance_flutter_3/core/domain/repository/payee/payee_repository.dart';
+import 'package:rxdart/subjects.dart';
 
 class PayeeListController {
   PayeeListController(this.payeeRepository);
 
   final PayeeRepository payeeRepository;
+
+  final payeeStream = BehaviorSubject<List<PayeeModel>>();
+
+  Future<void> start() async {
+    var payeeListStream = await payeeRepository.watchAll();
+    payeeStream.addStream(payeeListStream);
+  }
 
   Future deletePayee(PayeeModel payee) {
     return payeeRepository.delete(payee);
--- lib/ui/pages/tab_manager/payee/list/payee_list_page.dart
+++ lib/ui/pages/tab_manager/payee/list/payee_list_page.dart
@@ -5,6 +5,7 @@ import 'package:my_finance_flutter_3/ui/navigation/app_router.dart';
 import 'package:my_finance_flutter_3/ui/pages/tab_manager/payee/list/payee_list_controller.dart';
 import 'package:my_finance_flutter_3/ui/pages/tab_manager/payee/select/payee_select_page.dart';
 import 'package:my_finance_flutter_3/ui/widget/bottom_action_bar/bottom_action_bar.dart';
+import 'package:my_finance_flutter_3/ui/widget/wrapper/lifecycle_widget.dart';
 import 'package:provider/provider.dart';
 
 class PayeeListPage extends StatelessWidget {
@@ -16,8 +17,19 @@ class PayeeListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Provider(
       create: (context) => PayeeListController(context.read()),
-      builder: (context, _) {
-        return Scaffold(
+      child: LifecycleWidget(
+        key: const Key("PayeeListPage"),
+        onReady: (context) {
+          var controller = context.read<PayeeListController>();
+          controller.start();
+        },
+        onTopStack: (context) {
+          Log.i("PayeeListPage onTopStack");
+        },
+        onBackStack: (context) {
+          Log.i("PayeeListPage onBackStack");
+        },
+        child: Scaffold(
           body: BottomActionBar(
             actionList: [
               BottomActionItem(
@@ -34,21 +46,70 @@ class PayeeListPage extends StatelessWidget {
                 },
               ),
             ],
-            child: Consumer<List<PayeeModel>>(
-              builder: (context, value, child) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    var payee = value[index];
-
-                    return ListTile(
-                      title: Text(payee.name),
-                    );
-                  },
-                );
-              },
-            ),
+            child: const PayeeList(),
           ),
+        ),
+      ),
+      // builder: (context, _) {
+      //   return LifecycleWidget(
+      //     key: const Key("PayeeListPage"),
+      //     onReady: (context) {
+      //       var controller = context.read<PayeeListController>();
+      //       controller.start();
+      //     },
+      //     onTopStack: (context) {
+      //       Log.i("PayeeListPage onTopStack");
+      //     },
+      //     onBackStack: (context) {
+      //       Log.i("PayeeListPage onBackStack");
+      //     },
+      //     child: Scaffold(
+      //       body: BottomActionBar(
+      //         actionList: [
+      //           BottomActionItem(
+      //             icon: Icons.self_improvement_outlined,
+      //             onTap: () async {
+      //               var result = await showPayeeMultiSelector(context);
+      //               Log.i('Result - $result');
+      //             },
+      //           ),
+      //           BottomActionItem(
+      //             icon: Icons.add,
+      //             onTap: () {
+      //               AppRouter.navigateToPayeeForm(context);
+      //             },
+      //           ),
+      //         ],
+      //         child: const PayeeList(),
+      //       ),
+      //     ),
+      //   );
+      // },
+    );
+  }
+}
+
+class PayeeList extends StatelessWidget {
+  const PayeeList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var payeeList = context.read<PayeeListController>().payeeStream;
+    return StreamBuilder<List<PayeeModel>>(
+      stream: payeeList,
+      builder: (context, snapshot) {
+        var list = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            var payee = list[index];
+
+            return ListTile(
+              title: Text(payee.name),
+            );
+          },
         );
       },
     );
