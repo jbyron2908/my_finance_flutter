01cc33d1c0b63bd1b4a04f689ac91304dfab1270
Jonathan Byron <jonathan.byron2908@gmail.com>
2022-04-26 19:06:02 +0200

    wip

--- lib/ui/pages/tab_manager/profile/list/profile_list_controller.dart
+++ lib/ui/pages/tab_manager/profile/list/profile_list_controller.dart
@@ -1,12 +1,31 @@
 import 'package:my_finance_flutter_3/core/domain/model/profile/profile_model.dart';
 import 'package:my_finance_flutter_3/core/domain/repository/profile/profile_repository.dart';
+import 'package:rxdart/subjects.dart';
 
 class ProfileListController {
   ProfileListController(this.profileRepository);
 
   final ProfileRepository profileRepository;
 
+  final BehaviorSubject<ProfileListState> state =
+      BehaviorSubject.seeded(Loading());
+
+  void watch() async {
+    var profileStream = await profileRepository.watchAll();
+    state.add(Idle(profileStream));
+  }
+
   Future deleteProfile(ProfileModel profile) {
     return profileRepository.delete(profile);
   }
+}
+
+abstract class ProfileListState {}
+
+class Loading implements ProfileListState {}
+
+class Idle implements ProfileListState {
+  Idle(this.profileListStream);
+
+  final Stream<List<ProfileModel>> profileListStream;
 }
--- lib/ui/pages/tab_manager/profile/list/profile_list_page.dart
+++ lib/ui/pages/tab_manager/profile/list/profile_list_page.dart
@@ -5,6 +5,7 @@ import 'package:my_finance_flutter_3/ui/navigation/app_router.dart';
 import 'package:my_finance_flutter_3/ui/pages/tab_manager/profile/list/profile_list_controller.dart';
 import 'package:my_finance_flutter_3/ui/pages/tab_manager/profile/select/profile_select_page.dart';
 import 'package:my_finance_flutter_3/ui/widget/bottom_action_bar/bottom_action_bar.dart';
+import 'package:my_finance_flutter_3/ui/widget/wrapper/lifecycle_widget.dart';
 import 'package:provider/provider.dart';
 
 class ProfileListPage extends StatelessWidget {
@@ -17,36 +18,57 @@ class ProfileListPage extends StatelessWidget {
     return Provider(
       create: (context) => ProfileListController(context.read()),
       builder: (context, _) {
-        return Scaffold(
-          body: BottomActionBar(
-            actionList: [
-              BottomActionItem(
-                icon: Icons.self_improvement_outlined,
-                onTap: () async {
-                  var result = await showProfileMultiSelector(context);
-                  Log.i('Result - $result');
-                },
-              ),
-              BottomActionItem(
-                icon: Icons.add,
-                onTap: () {
-                  AppRouter.navigateToProfileForm(context);
-                },
-              ),
-            ],
-            child: Consumer<List<ProfileModel>>(
-              builder: (context, value, child) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    var profile = value[index];
+        ProfileListController controller = context.read();
 
-                    return ListTile(
-                      title: Text(profile.name),
-                    );
+        return LifecycleWidget(
+          onReady: (context) {
+            controller.watch();
+          },
+          child: Scaffold(
+            body: BottomActionBar(
+              actionList: [
+                BottomActionItem(
+                  icon: Icons.self_improvement_outlined,
+                  onTap: () async {
+                    var result = await showProfileMultiSelector(context);
+                    Log.i('Result - $result');
                   },
-                );
-              },
+                ),
+                BottomActionItem(
+                  icon: Icons.add,
+                  onTap: () {
+                    AppRouter.navigateToProfileForm(context);
+                  },
+                ),
+              ],
+              child: StreamBuilder<ProfileListState>(
+                stream: controller.state,
+                builder: (context, state) {
+                  if (state.data is Idle) {
+                    var idle = state.data as Idle;
+                    return StreamBuilder<List<ProfileModel>>(
+                      initialData: const [],
+                      stream: idle.profileListStream,
+                      builder: (context, snapshot) {
+                        var profileList = snapshot.requireData;
+
+                        return ListView.builder(
+                          itemCount: profileList.length,
+                          itemBuilder: (context, index) {
+                            var profile = profileList[index];
+
+                            return ListTile(
+                              title: Text(profile.name),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
             ),
           ),
         );
