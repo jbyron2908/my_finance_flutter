f17f54f9585e85f904ce69904179c57a6b0deb30
Jonathan Byron <12960860+jbyron2908@users.noreply.github.com>
2022-10-03 14:49:24 +0200

    Update dependencies

--- app/lib/ui/navigation/app_auto_router.dart
+++ app/lib/ui/navigation/app_auto_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:auto_route/empty_router_widgets.dart';
 import 'package:my_finance_flutter/ui/pages/main/main_page.dart';
 import 'package:my_finance_flutter/ui/pages/splash/splash_page.dart';
 import 'package:my_finance_flutter/ui/pages/tab_debug/debug/debug_tools_page.dart';
--- app/lib/ui/pages/tab_playground/playground_router.dart
+++ app/lib/ui/pages/tab_playground/playground_router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:auto_route/empty_router_widgets.dart';
 import 'package:my_finance_flutter/ui/pages/tab_playground/change_notifier/change_notifier_page.dart';
 import 'package:my_finance_flutter/ui/pages/tab_playground/counter/counter_change_notifier_page.dart';
 import 'package:my_finance_flutter/ui/pages/tab_playground/counter_state_notifier/counter_state_notifier_page.dart';
